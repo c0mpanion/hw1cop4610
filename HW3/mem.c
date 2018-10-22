aa61318bcb592c517f50577f032a8dc02f0a9b09
Jose Toledo <jats2k9@hotmail.com>
2018-10-22 17:31:26 -0400

    Changed implementation, Had a fundamental error

--- HW3/mem.c
+++ HW3/mem.c
@@ -2,16 +2,19 @@
 #include <sys/mman.h>
 #include <unistd.h>
 #include <math.h>
+#include <limits.h>
+#include <sys/fcntl.h>
 
 #define FIRST_FIT 0
 #define BEST_FIT 1
 #define WORST_FIT 2
 
 int policySet = FIRST_FIT; // default policy
-void *base;
-void *limit;
-struct node *head, *tail;
-
+uint8_t initFlag = 0;
+void *base = NULL;
+void *limit = NULL;
+unsigned long totalAllocated = 0;
+struct list *memoryList = NULL;
 
 struct node {
     void *address;
@@ -19,22 +22,38 @@ struct node {
     struct node *next;
 };
 
-int Mem_Init(int size, int policy) {
+struct list {
+    struct node *head;
+    unsigned long remainingMemory;
+};
 
+int Mem_Init(int size, int policy) {
+    if (initFlag) {
+        return -1;
+    }
+    initFlag = 1;
     policySet = policy;
+
     // round up to units of page size
     if (size < getpagesize() && size > 0) {
         size = getpagesize();
     } else {
-        size = (int) ceil(size / getpagesize()) * getpagesize();
+        size = (int) ceil((double) size / (double) getpagesize()) * getpagesize();
     }
 
-    if ((base = mmap(NULL, (size_t) size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0)) ==
-        (caddr_t) -1) {
+    // open the /dev/zero device
+    int fd = open("/dev/zero", O_RDWR);
+    // size (in bytes) must be divisible by page size
+    base = mmap(NULL, (size_t) size, PROT_READ | PROT_WRITE,
+                MAP_PRIVATE, fd, 0);
+    if (base == MAP_FAILED) {
+        perror("mmap");
         return -1;
     }
     limit = base + size;
-    printf("\n%p", limit);
+    totalAllocated = (unsigned long) size;
+    // close the device (don't worry, mapping should be unaffected)
+    close(fd);
     return 0;
 }
 
@@ -44,130 +63,166 @@ void *Mem_Alloc(int size) {
     struct node *curr;
     struct node *next;
 
-    if (head != NULL) {
-        curr = head;
-        next = head->next;
+    if (memoryList != NULL) {
+        //if requested is greater than remaining
+        if (memoryList->remainingMemory < size) {
+            return NULL;
+        }
+
+        curr = memoryList->head;
+        next = curr->next;
 
         if (policySet == FIRST_FIT) {
-            //check if there is space between base and head of list
-            if ((void *) curr - base > size) {
-                struct node *temp = (struct node *) base;
-                temp->address = sizeof(struct node) + base;
+            //if fit at head of list
+            if ((curr->address - base) > size) {
+                int fd = open("/dev/zero", O_RDWR);
+                struct node *temp = mmap(NULL, sizeof(struct node), PROT_READ | PROT_WRITE,
+                                         MAP_PRIVATE, fd, 0);
+                temp->address = base;
                 temp->size = size;
-                head = temp;
+                temp->next = curr;
+                memoryList->head = temp;
+                memoryList->remainingMemory = memoryList->remainingMemory - size;
                 return temp->address;
             }
 
+            //find space in between nodes
             while (curr->next != NULL) {
-                // if there is enough space between nodes, insert here
-                if (((void *) next - ((void *) curr + sizeof(struct node) + curr->size)) >
-                    (void *) (sizeof(struct node) + size)) {
-                    struct node *temp = (curr + sizeof(struct node) + curr->size);
-                    temp->address = ((void *) temp + sizeof(struct node));
+                if (((next->address) - (curr->address + curr->size)) > size) {
+                    int fd = open("/dev/zero", O_RDWR);
+                    struct node *temp = mmap(NULL, sizeof(struct node), PROT_READ | PROT_WRITE,
+                                             MAP_PRIVATE, fd, 0);
+                    temp->address = curr->address + curr->size;
                     temp->size = size;
                     temp->next = next;
                     curr->next = temp;
+                    memoryList->remainingMemory = memoryList->remainingMemory - size;
                     return temp->address;
-                } else {
-                    curr = curr->next;
-                    next = next->next;
                 }
-            }
-            //if limit address has no been reached
-            if ((((void *) curr + curr->size + sizeof(struct node)) + sizeof(struct node) + size) < limit) {
-                curr->next = ((void *) curr + curr->size + sizeof(struct node));
                 curr = curr->next;
-                curr->address = (sizeof(struct node) + (void *) curr);
-                curr->size = size;
-                return curr->address;
+                next = next->next;
             }
-            return NULL;
-
+            // space found at end
+            int fd = open("/dev/zero", O_RDWR);
+            struct node *temp = mmap(NULL, sizeof(struct node), PROT_READ | PROT_WRITE,
+                                     MAP_PRIVATE, fd, 0);
+            temp->address = curr->address + curr->size;
+            temp->size = size;
+            temp->next = next;
+            curr->next = temp;
+            memoryList->remainingMemory = memoryList->remainingMemory - size;
+            return temp->address;
 
         } else if (policySet == BEST_FIT) {
 
         } else if (policySet == WORST_FIT) {
 
         }
-    } else {
-        head = (struct node *) base;
-        // as far as the user is concern, this is the address of his object.
-        head->address = sizeof(struct node) + base;
-        head->size = size;
-        return head->address;
+    }
+        //memoryList is empty and requested fits
+    else if (size < (limit - base)) {
+        int fd = open("/dev/zero", O_RDWR);
+        int fd2 = open("/dev/zero", O_RDWR);
+
+        memoryList = mmap(NULL, sizeof(struct list), PROT_READ | PROT_WRITE,
+                          MAP_PRIVATE, fd, 0);
+        memoryList->head = mmap(NULL, sizeof(struct node), PROT_READ | PROT_WRITE,
+                                MAP_PRIVATE, fd2, 0);
+        memoryList->remainingMemory = totalAllocated - size;
+        memoryList->head->next = NULL;
+        memoryList->head->address = base;
+        memoryList->head->size = size;
+        return memoryList->head->address;
     }
     return NULL;
 }
 
-
+//
 int Mem_Free(void *ptr) {
     if (ptr == NULL) {
         return 0;
     }
-
-    struct node *curr = head;
-    struct node *prev = head;
-
-    // if pointer is at head of list
+    struct node *curr = memoryList->head;
+    struct node *prev = memoryList->head;
+    // if pointer is at head of memoryList
     if (curr->address <= ptr && ptr < (curr->address + curr->size)) {
-        head = head->next;
+        memoryList->remainingMemory = memoryList->remainingMemory + curr->size;
+        struct node *temp = memoryList->head->next;
+        munmap(curr, sizeof(struct node));
+        memoryList->head = temp;
         return 0;
     }
-
-    // if pointer is within a node, remove node form list
+    // if pointer is within a node, remove node form memoryList
     curr = curr->next;
     while (curr != NULL) {
         if (curr->address <= ptr && ptr < (curr->address + curr->size)) {
+            memoryList->remainingMemory = memoryList->remainingMemory + curr->size;
             prev->next = curr->next;
+            munmap(curr, sizeof(struct node));
             return 0;
         }
+        prev = prev->next;
         curr = curr->next;
     }
     return -1;
 }
-
-int Mem_IsValid(void *ptr) {
-    if (ptr == NULL) {
-        return 0;
-    }
-    struct node *curr = head;
-    while (curr != NULL) {
-        if (curr->address <= ptr && ptr < (curr->address + curr->size)) {
-            return 1;
-        }
-        curr = curr->next;
-    }
-    return 0;
-}
+//
+//int Mem_IsValid(void *ptr) {
+//    if (ptr == NULL) {
+//        return 0;
+//    }
+//    struct node *curr = head;
+//    while (curr != NULL) {
+//        if (curr->address <= ptr && ptr < (curr->address + curr->size)) {
+//            return 1;
+//        }
+//        curr = curr->next;
+//    }
+//    return 0;
+//}
 
 int main(int argc, char *argv[]) {
 
     if (!Mem_Init(1, 0)) {
-        printf("\nBASE >> %p\n", base);
+        printf("\nBASE >> %p, LIMIT %p\n", base, limit);
     } else {
         printf("Failed");
     }
-    printf("\nSIZE >> %d\n\n", (int) sizeof(struct node));
+    if (!Mem_Init(66000, 0)) {
+        printf("\nBASE >> %p, LIMIT %p\n", base, limit);
+    } else {
+        printf("Failed");
+    }
+    printf("\nSIZE >> %zu\n\n", sizeof(struct node));
 
-    void *p = Mem_Alloc(50);
-    printf("\nP > %p\n", p);
-    void *x = Mem_Alloc(20);
-    printf("\nX > %p\n", x);
 
 
-    void *c = Mem_Alloc(1);
-    printf("\nC > %p\n", c);
-    void *v = Mem_Alloc(1);
-    printf("\nV > %p\n", v);
 
-    printf("FREE %d\n", Mem_Free(p));
-    printf("IS VALID %d\n", Mem_IsValid(p));
+//**********************************************************************
 
-    void *z = Mem_Alloc(65535);
-    void *b = Mem_Alloc(10);
+    void *p = Mem_Alloc(20);
+    printf("\nP > %lu\n", (unsigned long) p);
+    printf("\nPAGE SIZE > %d\n", getpagesize());
 
-    printf("IS VALID %d\n", Mem_IsValid(z));
-    printf("IS VALID %d\n", Mem_IsValid(b));
+    void *a = Mem_Alloc(1);
+    printf("\nA > %lu\n", (unsigned long) a);
 
+    void *z = Mem_Alloc(2);
+    printf("\nZ > %lu\n", (unsigned long) z);
+
+    printf("FREE P %lu\n", Mem_Free(p));
+
+
+    void *l = Mem_Alloc(10);
+    printf("\nL > %lu\n", (unsigned long) l);
+
+
+    void *N = Mem_Alloc(50);
+    printf("\nN > %lu\n", (unsigned long) N);
+
+////**********************************************************************
+
+
+    munmap(base, limit - base);
 
 }
